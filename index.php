572868d8bf5ef59bd75c44df2e4fea0b74685922
ktz51 <30367605+ktz51@users.noreply.github.com>
2017-08-10 09:23:06 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Goldxengenharia</title>
+</head>
+<!--<style>@import url('/css/styles.css');</style>-->
+<link rel="stylesheet" type="text/css" href=".\css\index.css">
+<!--<script type="text/javascript" src="script.js"></script>-->
+<script src="js/index.js"></script>
+
+<body onload="desc()">
+<pre>
+<?php
+//$ceo=$_GET['ceo'];
+$ceo="<strong>Alexsander rebouças";
+$partiner=" Alexsey rebouças";
+echo "$ceo <br &emsp; &emsp;  &emsp; &emsp;<br>$partiner\n";
+//<>
+//!= 
+ ?>
+
+
+
+
+
+														
+																	
+
+													
+
+
+
+
+
+
+
+
+
+													<form method="get" action=".\\dados.php">
+														Username <input type="text" name="name" id="" style="background-color:transparent;" > Password <input type="password" name="pass" id="" style="background-color:transparent; "> <input type=submit value="acessar" />
+													</form>
+
+<!--
+<div id="inputs">
+<input type="text" name="" id="" style="background-color:transparent; ">usernsme<input type="password" name="" id="">
+</div>
+-->
+</pre>
+ </body>
+</html>
